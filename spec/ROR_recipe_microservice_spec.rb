9f078241ca28bb6dcc2e78df6f65331cef062d13
Trevor Robinson <trevor.robi1254@gmail.com>
2021-04-17 09:38:10 -0600

    Setup sinatra with rspec
    Co-authored-by: Arika Blenker <71908665+arikalea@users.noreply.github.com>
    Co-authored-by: Harrison Blake <72946334+harrison-blake@users.noreply.github.com>
    Co-authored-by: Hope McGee <52016184+hopesgit@users.noreply.github.com>
    Co-authored-by: Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>
    Co-authored-by: Jeremiah Michlitsch <61896482+jmichlitsch@users.noreply.github.com>
    Co-authored-by: Megan Gonzales <62969459+MGonzales26@users.noreply.github.com>

---
+++ spec/ROR_recipe_microservice_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+RSpec.describe RecipeMicroserviceController, type: :request do
+
+   it 'returns hello world' do
+
+    get '/'
+    body = JSON.parse(last_response.body, symbolize_names: true)
+    expect(body[:message]).to eq("Hello World!")
+  end
+end  
